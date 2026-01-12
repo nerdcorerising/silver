29e198f820fe8b74822b650a22c3cf4bb736bfae
David Mason <nerdcorerising@gmail.com>
2026-01-11 18:58:23 -0600

    Cleanup unused code

--- triplets/x64-windows-silver.cmake
+++
@@ -1,7 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-# Disable vectorized STL algorithms to avoid linker issues with satellite libraries
-set(VCPKG_CXX_FLAGS "/D_DISABLE_VECTOR_ANNOTATION /D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR")
-set(VCPKG_C_FLAGS "")
